2596d9241e88684f6fa2febd9cc1428182ca05af
vmuser <vmuser@scale1j2mB7C9AO>
2023-09-12 09:31:26 +0000

    Route contact and aboutus

--- evrentals/src/App.js
+++ evrentals/src/App.js
@@ -1,15 +1,17 @@
 import LoginForm from './loginpage/loginform';
 import RegistrationForm from './registratn_comp/registrationform';
 import HomePage from './homepage/home';
+import Aboutus from './aboutus';
 import {Routes,Route, BrowserRouter as Router} from "react-router-dom";
 function App() {
   return (
     <Router>
       <Routes>
       <Route path="*" element={<HomePage />} />
-      <Route path="signup" element={<RegistrationForm />} />
+      {/* <Route path="signup" element={<RegistrationForm />} /> */}
+      <Route path='/registrationform' element={<RegistrationForm></RegistrationForm>}/> 
       <Route exact path="/login" element={<LoginForm />} />
-
+      <Route path='/aboutus' element={<Aboutus/>}></Route>
         </Routes>
       </Router>
     
